ec0a1ce5b64a85c650b7b88e3068d81137d9aa0a
Chewy42 <mfavela@chapman.edu>
2025-11-30 18:05:04 +0000

    profile setup script

---
+++ git_profile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# this file is just for me to load my git credentials in the replti env
+
+git config --global user.email "mfavela@chapman.edu"
+git config --global user.name "Chewy42"
